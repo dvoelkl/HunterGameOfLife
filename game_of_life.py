2656016b2c286c4168ca6260c3f146f559ecea27
dvoelkl <135894963+dvoelkl@users.noreply.github.com>
2025-03-14 13:24:05 +0100

    Added Spawners
    Added Blocks, that can spawn cells with a specific probability

--- game_of_life.py
+++ game_of_life.py
@@ -7,21 +7,24 @@ from matplotlib.colors import ListedColormap
 # Parameters
 WORLD_SIZE = 125
 EPOCHS = 1000
-HUNGER_LIMIT = 10
+HUNGER_LIMIT = 11
 init_p = [0.001, 0.2, 0.799]  # Probabilities for [HUNTER, ALIVE, DEAD]
 HUNTER_REPRO_RATE = 1
 CANIBALISM_INTERVAL = 3
 interpolation = 'nearest'
+HUNTER_SPAWNER_PROB = 0.001
+CELL_SPAWNER_PROB = 0.01
 
 # Celltypes
 DEAD = 0
 ALIVE = 1
 HUNTER = 2
-BORDER = 99
+BORDER = 3
+HUNTER_SPAWNER = 4
+CELL_SPAWNER = 5
 
 
-
-world = np.random.choice([ALIVE, DEAD], size=(WORLD_SIZE, WORLD_SIZE), p=[0.5, 0.5])
+world = np.random.choice([ALIVE, DEAD], size=(WORLD_SIZE, WORLD_SIZE), p=[0.3, 0.7])
 hunger_levels = np.zeros((WORLD_SIZE, WORLD_SIZE), dtype=int)
 
 def count_neighbors(world, x, y):
@@ -41,12 +44,7 @@ def count_neighbors(world, x, y):
 
 def eat_neighbors_and_reproduce_or_starve(world, hunger_levels, x, y):
     found_Food = False
-    left = (x - 1) % WORLD_SIZE
-    right = (x + 1) % WORLD_SIZE
-    up = (y - 1) % WORLD_SIZE
-    down = (y + 1) % WORLD_SIZE
-    
-    neighbors = [(left, up), (x, up), (right, up), (left, y), (right, y), (left, down), (x, down), (right, down)]
+    neighbors = get_neighbours(x, y)
 
     possible_snacks = []
     for nx, ny in neighbors:
@@ -89,6 +87,15 @@ def eat_neighbors_and_reproduce_or_starve(world, hunger_levels, x, y):
             hunger_levels[new_x, new_y] = hunger_levels[x, y]
             hunger_levels[x, y] = 0
 
+def get_neighbours(x, y):
+    left = (x - 1) % WORLD_SIZE
+    right = (x + 1) % WORLD_SIZE
+    up = (y - 1) % WORLD_SIZE
+    down = (y + 1) % WORLD_SIZE
+    
+    neighbors = [(left, up), (x, up), (right, up), (left, y), (right, y), (left, down), (x, down), (right, down)]
+    return neighbors
+
 def reproduce(world, hunger_levels):
     new_world = np.copy(world)
     new_hunger_levels = np.copy(hunger_levels)
@@ -105,6 +112,32 @@ def reproduce(world, hunger_levels):
                 elif world[x, y] == DEAD:
                     if cnt_nbrs == 3:
                         new_world[x, y] = ALIVE
+                elif world[x, y] == HUNTER_SPAWNER:
+                    if np.random.rand() < HUNTER_SPAWNER_PROB:
+                        neighbours = get_neighbours(x, y)
+                        poss_pos = []
+                        for nx, ny in neighbours:
+                            if world[nx, ny] == DEAD:
+                                poss_pos.append((nx, ny))
+                        
+                        if len(poss_pos) > 0:
+                            for _ in range(random.choice(range(len(poss_pos)))):
+                                new_x, new_y = random.choice(poss_pos)
+                                new_world[new_x, new_y] = HUNTER
+                                new_hunger_levels[new_x, new_y] = 0
+
+                elif world[x, y] == CELL_SPAWNER:
+                    if np.random.rand() < CELL_SPAWNER_PROB:
+                        neighbours = get_neighbours(x, y)
+                        poss_pos = []
+                        for nx, ny in neighbours:
+                            if world[nx, ny] == DEAD:
+                                poss_pos.append((nx, ny))
+                        
+                        if len(poss_pos) > 0:
+                            for _ in range(random.choice(range(len(poss_pos)))): # Spawn a random amount of new cells at the same time
+                                new_x, new_y = random.choice(poss_pos)
+                                new_world[new_x, new_y] = ALIVE
     
     return new_world, new_hunger_levels
 
@@ -125,6 +158,9 @@ if __name__ == '__main__':
     world[25, int(WORLD_SIZE/2)+25:] = BORDER
     world[-25, :int(WORLD_SIZE/2)-25] = BORDER
     world[-25, int(WORLD_SIZE/2)+25:] = BORDER
+    world[25, :int(WORLD_SIZE/2)-15]= HUNTER_SPAWNER
+    world[0:25, 0:25]= np.random.choice([CELL_SPAWNER, DEAD], size=(25, 25), p=[0.05, 0.95])
+
     #world = np.random.choice([ALIVE, DEAD], size=(WORLD_SIZE, WORLD_SIZE), p=[0.2, 0.8])
     # set upper triagonal to hunters
     #for i in range(WORLD_SIZE):
@@ -141,10 +177,8 @@ if __name__ == '__main__':
         im.set_array(world)
         return [im]
     
-    cmap = ListedColormap(['white', 'green', 'red', 'black'])
-    im = ax.imshow(world, vmin=0, vmax=3, cmap=cmap, interpolation=interpolation)
+    cmap = ListedColormap(['white', 'green', 'red', 'black', 'purple', 'blue'])
+    im = ax.imshow(world, vmin=0, vmax=5, cmap=cmap, interpolation=interpolation)
     ani = animation.FuncAnimation(fig, update_world, frames=EPOCHS, interval=100, repeat=False)
-    
-    plt.show()
     
     plt.show()
